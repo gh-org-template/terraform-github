114971767b1582ecad1fbb502e58785901160e8e
Colin Hutchinson <chutchic@gmail.com>
2024-07-24 15:21:18 -0400

    feat(ruleset): define org-wide ruleset

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,34 @@
-resource "local_file" "hello_world" {
-  content  = "Hello, World!"
-  filename = "${path.module}/hello_world.txt"
+# main.tf
+
+resource "github_organization_ruleset" "pr_ruleset" {
+  name        = "require-pull-requests"
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+    repository_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+  }
+
+  rules {
+    creation                = true
+    update                  = true
+    deletion                = true
+    required_linear_history = true
+    required_signatures     = true
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = false
+      require_code_owner_review         = false
+      require_last_push_approval        = false
+      required_approving_review_count   = 1
+      required_review_thread_resolution = false
+    }
+  }
 }
