a06daddfe4c855aad3274d672ecb7076b826a3b6
Colin Hutchinson <chutchic@gmail.com>
2024-07-24 15:15:34 -0400

    feat(github): setup github as a provider

---
+++ terraform/backend.tf
@@ -0,0 +1,12 @@
+# backend.tf
+
+terraform {
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "gh-org-template/template-1-terraform"
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "http" {
-    address        = "https://api.tfstate.dev/github/v1"
-    lock_address   = "https://api.tfstate.dev/github/v1/lock"
-    unlock_address = "https://api.tfstate.dev/github/v1/lock"
-    lock_method    = "PUT"
-    unlock_method  = "DELETE"
-    username       = "gh-org-template/terraform-github"
-  }
-}
-
 resource "local_file" "hello_world" {
   content  = "Hello, World!"
   filename = "${path.module}/hello_world.txt"
