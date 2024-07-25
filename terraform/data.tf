0be15cc3bc9940bcc4c4c7f9161cfed0412c7b49
Colin Hutchinson <chutchic@gmail.com>
2024-07-24 21:48:05 -0400

    feat(ruleset): require the terraform check to pass on terraform repositories

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,3 +3,7 @@
 data "github_repositories" "all_repos" {
   query = "org:${var.github_org}"
 }
+
+data "github_repositories" "terraform_repos" {
+  query = "org:${var.github_org} terraform in:name"
+}
