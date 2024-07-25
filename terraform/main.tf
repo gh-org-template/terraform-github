8fccd66e1220d241623a36f1482b900a546290a7
Colin Hutchinson <chutchic@gmail.com>
2024-07-24 21:48:05 -0400

    feat(ruleset): require the terraform check to pass on terraform repositories

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 # main.tf
 
-resource "github_repository_ruleset" "pr_ruleset" {
+resource "github_repository_ruleset" "pr_ruleset_all" {
   for_each    = toset(data.github_repositories.all_repos.names)
-  name        = "protect-main-branch"
+  name        = "protect-main-branch-${each.key}"
   repository  = each.key
   target      = "branch"
   enforcement = "active"
@@ -32,6 +32,33 @@ resource "github_repository_ruleset" "pr_ruleset" {
     required_status_checks {
       required_check {
         context = "pre-commit"
+      }
+      strict_required_status_checks_policy = true
+    }
+  }
+}
+
+resource "github_repository_ruleset" "pr_ruleset_terraform" {
+  for_each    = toset(data.github_repositories.terraform_repos.names)
+  name        = "protect-main-branch-terraform-${each.key}"
+  repository  = each.key
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/main"]
+      exclude = []
+    }
+  }
+
+  rules {
+    required_status_checks {
+      required_check {
+        context = "pre-commit"
+      }
+      required_check {
+        context = "terraform"
       }
       strict_required_status_checks_policy = true
     }
