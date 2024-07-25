6ad773ac0a42607c3735f5f239d332e3142156d3
Colin Hutchinson <hutchic@users.noreply.github.com>
2024-07-24 22:51:13 -0400

    feat(repo): set the default repository settings (#13)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,19 @@
 # main.tf
 
+# Apply repository settings to all repositories
+resource "github_repository" "settings_all" {
+  for_each = toset(data.github_repositories.all_repos.names)
+  name     = each.key
+
+  has_issues             = false
+  has_wiki               = false
+  has_projects           = false
+  allow_merge_commit     = false
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+}
+
+# Apply ruleset to all repositories
 resource "github_repository_ruleset" "pr_ruleset_all" {
   for_each    = toset(data.github_repositories.all_repos.names)
   name        = "protect-main-branch-${each.key}"
@@ -24,6 +38,7 @@ resource "github_repository_ruleset" "pr_ruleset_all" {
   }
 }
 
+# Apply ruleset to repositories with 'terraform' in their name
 resource "github_repository_ruleset" "pr_ruleset_terraform" {
   for_each    = toset(data.github_repositories.terraform_repos.names)
   name        = "protect-main-branch-terraform-${each.key}"
