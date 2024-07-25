faa1ee1bbcf37b4d3287ec3db431913efc1b59fe
Colin Hutchinson <chutchic@gmail.com>
2024-07-25 16:04:45 -0400

    feat(kong): all kong repositories complete their build via the 'done' stage

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,3 +7,7 @@ data "github_repositories" "all_repos" {
 data "github_repositories" "terraform_repos" {
   query = "org:${var.github_org} terraform in:name"
 }
+
+data "github_repositories" "kong_repos" {
+  query = "org:${var.github_org} kong in:name"
+}
