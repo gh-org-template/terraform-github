c686d4c2eecb47ca4d548394beeb42f2c58d7acc
Colin Hutchinson <chutchic@gmail.com>
2024-08-28 14:38:58 -0400

    fix(private): private v visibility toggle

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "github_repository" "settings_all" {
   for_each = { for repo in var.repositories : repo.name => repo }
   name     = each.key
 
-  visibility             = "private"
+  private                = lookup(each.value, "private", false) # Toggle repository privacy using 'private'
   has_issues             = false
   has_wiki               = false
   has_projects           = false
